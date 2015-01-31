8edbff499ab79c8e8504effb346447b79771df7b
ironholds <ironholds@gmail.com>
2015-01-31 12:34:57 -0500

    set custom user agent

--- R/utilities.R
+++ R/utilities.R
@@ -1,12 +1,12 @@
 #Backend for all API calls.
-#'@importFrom httr stop_for_status content GET
+#'@importFrom httr stop_for_status content GET user_agent
 query <- function(url_params, ...){
   if(grepl(x = url_params, pattern = "http")){
     url <- url_params
   } else {
     url <- paste0("http://swapi.co/api", url_params)
   }
-  result <- GET(url, ...)
+  result <- GET(url, user_agent("rwars - https://github.com/Ironholds/rwars"), ...)
   stop_for_status(result)
   return(content(result))
 }
