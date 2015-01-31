845f956ba44ac83ad0dab2d1e5c6c609bf196cfc
ironholds <ironholds@gmail.com>
2015-01-31 11:25:16 -0500

    more documentation, add query continuation for *_all

--- R/all.R
+++ R/all.R
@@ -1,59 +1,101 @@
 #'@title Retrieve the data associated with every entry in a class.
 #'@rdname all_entries
 #'@aliases all_entries
+#'
+#'@param query_continue whether this is a continuation of a previous query. Set to
+#'NULL by default; the alternative is to provide this argument with the "next" URL
+#'from a returned object - see the examples.
+#'
+#'@param parse_result whether to parse and simplify the result of the query.
+#'Set to FALSE by default.
+#'
+#'@param ... further arguments to pass to httr's GET function.
+#'
+#'@examples
+#'#Retrieve all planets, parsing
+#'planets <- get_all_planets(parse_result = TRUE)
+#'
+#'#It's only returned some of the results! Use query_continue to get
+#'#the rest.
+#'second_set_of_planets <- get_all_planets(getElement(planets,"next"), parse_result = TRUE)
+#'
 #'@export
-get_all_planets <- function(parse_result = FALSE, ...){
-  result <- query("/planets/", ...)
+get_all_planets <- function(query_continue = NULL, parse_result = FALSE, ...){
+  if(is.null(query_continue)){
+    result <- query("/planets/", ...)
+  } else {
+    result <- query(query_continue, ...)
+  }
   if(parse_result){
     result$results <- lapply(result$results,planet_entry_parse)
   }
   return(result)
 }
 
-#'@rdname entries
+#'@rdname all_entries
 #'@export
-get_all_species <- function(parse_result = FALSE, ...){
-  result <- query("/species/", ...)
+get_all_species <- function(query_continue = NULL, parse_result = FALSE, ...){
+  if(is.null(query_continue)){
+    result <- query("/species/", ...)
+  } else {
+    result <- query(query_continue, ...)
+  }
   if(parse_result){
     result$results <- lapply(result$results,species_entry_parse)
   }
   return(result)
 }
 
-#'@rdname entries
+#'@rdname all_entries
 #'@export
-get_all_films <- function(parse_result = FALSE, ...){
-  result <- query("/films/", ...)
+get_all_films <- function(query_continue = NULL, parse_result = FALSE, ...){
+  if(is.null(query_continue)){
+    result <- query("/films/", ...)
+  } else {
+    result <- query(query_continue, ...)
+  }
   if(parse_result){
     result$results <- lapply(result$results,film_entry_parse)
   }
   return(result)
 }
 
-#'@rdname entries
+#'@rdname all_entries
 #'@export
-get_all_vehicles <- function(parse_result = FALSE, ...){
-  result <- query("/vehicles/", ...)
+get_all_vehicles <- function(query_continue = NULL, parse_result = FALSE, ...){
+  if(is.null(query_continue)){
+    result <- query("/vehicles/", ...)
+  } else {
+    result <- query(query_continue, ...)
+  }
   if(parse_result){
     result$results <- lapply(result$results,vehicle_entry_parse)
   }
   return(result)
 }
 
-#'@rdname entries
+#'@rdname all_entries
 #'@export
-get_all_starships <- function(parse_result = FALSE, ...){
-  result <- query("/starships/", ...)
+get_all_starships <- function(query_continue = NULL, parse_result = FALSE, ...){
+  if(is.null(query_continue)){
+    result <- query("/starships/", ...)
+  } else {
+    result <- query(query_continue, ...)
+  }
   if(parse_result){
     result$results <- lapply(result$results,vehicle_entry_parse)
   }
   return(result)
 }
 
-#'@rdname entries
+#'@rdname all_entries
 #'@export
-get_all_people <- function(parse_result = FALSE, ...){
-  result <- query("/people/", ...)
+get_all_people <- function(query_continue = NULL, parse_result = FALSE, ...){
+  if(is.null(query_continue)){
+    result <- query("/people/", ...)
+  } else {
+    result <- query(query_continue, ...)
+  }
   if(parse_result){
     result$results <- lapply(result$results,person_entry_parse)
   }
